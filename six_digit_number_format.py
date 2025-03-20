c9b8798fc14cd5c941253b7a6151aba709addf7c
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-20 20:52:12 +0800

    formatted input error

---
+++ six_digit_number_format.py
@@ -0,0 +1,8 @@
+
+number = input("Enter a number from 0-1000: ") # Taking input from user
+
+#Formatting the number to 6 digits 
+formatted_number = "{:06d}".format(number)
+
+print("Formatted number: ", formatted_number) # Printing the formatted number
+
