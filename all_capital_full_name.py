a89e5ce03d7231cbc541bf38adcca7e34ad107b2
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-20 21:04:57 +0800

    minor changes, and all capital done

---
+++ all_capital_full_name.py
@@ -0,0 +1,9 @@
+
+#Asks the user to enter their full name 
+full_name = input("Please enter your full name: ")
+
+#Converts the full name to all capital letters
+capital_name = full_name.upper()
+
+#Prints the full name in all capital letters
+print(capital_name)
--- six_digit_number_format.py
+++ six_digit_number_format.py
@@ -4,5 +4,5 @@ number = int(input("Enter a number from 0-1000: ")) # Taking input from user
 #Formatting the number to 6 digits 
 formatted_number = "{:06d}".format(number)
 
-print("Formatted number: ", formatted_number) # Printing the formatted number
+print(formatted_number) # Printing the formatted number
 
