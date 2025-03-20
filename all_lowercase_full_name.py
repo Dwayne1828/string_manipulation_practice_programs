eb9f2485e955021197cc84d1210a47fccfbc8d48
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-20 21:14:23 +0800

    lowercase name print

--- all_lowercase_full_name.py
+++ all_lowercase_full_name.py
@@ -4,3 +4,6 @@ full_name = input("Please enter your full name: ")
 
 #Converts the full name to all lowercase letters
 lowercase_name = full_name.lower()
+
+#Prints the full name in all lowercase letters
+print(lowercase_name)
