0b7ffd98355b90f2ece45357fbedf6c67cefb743
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-21 07:49:46 +0800

    .title to fixed casing

---
+++ incorrect_to_pascal_casing.py
@@ -0,0 +1,6 @@
+
+#Asks the user for incorrect casing full name
+incorrect_case_name = input("Please enter full name in incorrect casing: ")
+
+#Corrects the casing of the full name
+correct_case_name = incorrect_case_name.title()
