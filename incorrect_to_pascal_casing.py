67c727df2bde5708ddf93ebdaba8a210314143f9
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-21 07:53:02 +0800

    pascal case error

--- incorrect_to_pascal_casing.py
+++ incorrect_to_pascal_casing.py
@@ -4,3 +4,6 @@ incorrect_case_name = input("Please enter full name in incorrect casing: ")
 
 #Corrects the casing of the full name
 correct_case_name = incorrect_case_name.title()
+pascal_case_name = "".join(correct_case_name)
+
+print(pascal_case_name)
