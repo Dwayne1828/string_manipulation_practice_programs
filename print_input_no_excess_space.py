c243f8111c201d08d2ea5d230b66c8bb4fdda1a1
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-20 19:58:27 +0800

    process for no excess space name

---
+++ print_input_no_excess_space.py
@@ -0,0 +1,5 @@
+
+full_name = input("Enter your full name with several spaces at the beginning: ")  # Input: "   John Doe"
+
+# Remove the excess spaces at the beginning of the input
+full_name = full_name.strip()
