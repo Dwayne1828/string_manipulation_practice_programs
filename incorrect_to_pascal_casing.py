c275c2c5bc047ab6e01c697c25ce6cb4e5112196
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-21 07:56:29 +0800

    pascal case fixed with .split

--- incorrect_to_pascal_casing.py
+++ incorrect_to_pascal_casing.py
@@ -4,6 +4,6 @@ incorrect_case_name = input("Please enter full name in incorrect casing: ")
 
 #Corrects the casing of the full name
 correct_case_name = incorrect_case_name.title()
-pascal_case_name = "".join(correct_case_name)
+pascal_case_name = "".join(correct_case_name.split())
 
 print(pascal_case_name)
