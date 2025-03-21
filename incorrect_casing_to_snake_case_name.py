8f4b8ae919f6aca3d8e762bf04913ae6128618ea
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-21 08:00:54 +0800

    lower the casing for snake case

---
+++ incorrect_casing_to_snake_case_name.py
@@ -0,0 +1,8 @@
+
+#Asks the user to enter their full name
+full_name = input("Please enter your full name: ")
+
+#Using .lower() to convert the full name to lowercase first 
+full_name = full_name.lower()
+
+#Then using .join to join the full name with underscore
